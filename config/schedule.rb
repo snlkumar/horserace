75ce8d36d1dfd274388031123c5b1457ce506669
sunil <snlkumar447@gmail.com>
2014-01-10 18:29:40 +0530

    commit after some changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,7 +12,7 @@
 
 #
 # every 2.hours do
-#   command "/usr/bin/some_great_command"
+#   command "/usr/bin/some_great_command"      
 #   runner "MyModel.some_method"
 #   rake "some:great:rake:task"
 # end
