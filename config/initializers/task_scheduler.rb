81a7a33cff8d03b30ee8d13d4bc50db2c2edca0b
sunil <snlkumar447@gmail.com>
2014-03-14 12:02:34 +0530

    commit after write all cron job for rufus scheduler

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,7 +1,30 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.new
-scheduler.every("10s") do
+scheduler.every("9m") do
    Race.bet_reminder
 end
+# scheduler.every 1.day ,:at=>'04:15pm' do
+   # runner "Client.check_trail_duration"
+# end
+scheduler.cron '15 16 * * *' do
+  Client.update_daily_bet_amount
+end
+scheduler.cron '1 0 * * *' do
+  Client.update_daily_bet_amount
+end
+# scheduler.every 1.day, :at => '12:01 am' do
+  # runner "Client.update_daily_bet_amount"
+# end
+
+# scheduler.every 9.minutes do
+  # runner "Race.bet_reminder"
+# end
+
+scheduler.every '0 0 1 * * ' do
+ ClinetFee.calculate_fee
+end
+# scheduler.every 1.month do
+  # runner 'ClinetFee.calculate_fee'
+# end
 # scheduler.join
--- config/schedule.rb
+++ config/schedule.rb
@@ -20,17 +20,17 @@
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
-every 1.day ,:at=>'04:15pm' do
-   runner "Client.check_trail_duration"
-end
-every 1.day, :at => '12:01 am' do
-  runner "Client.update_daily_bet_amount"
-end
-
-every 9.minutes do
-  runner "Race.bet_reminder"
-end
-every 1.month do
-  runner 'ClinetFee.calculate_fee'
-end
+# every 1.day ,:at=>'04:15pm' do
+   # runner "Client.check_trail_duration"
+# end
+# every 1.day, :at => '12:01 am' do
+  # runner "Client.update_daily_bet_amount"
+# end
+# 
+# every 9.minutes do
+  # runner "Race.bet_reminder"
+# end
+# every 1.month do
+  # runner 'ClinetFee.calculate_fee'
+# end
 # Learn more: http://github.com/javan/whenever
